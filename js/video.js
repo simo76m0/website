8108976b12dc0dd1e6c21d847791d5042a81b8b1
Simone Pedersen <simonemmpedersen@gmail.com>
2023-01-10 13:37:19 +0100

    javascript video faster slower

--- js/video.js
+++ js/video.js
@@ -4,22 +4,37 @@ function start() {
   const video = document.querySelector("#video_file");
   const playBtn = document.querySelector("#play");
   const stopBtn = document.querySelector("#stop");
+  const slowBtn = document.querySelector("#slowdown");
+  const speedBtn = document.querySelector("#speedup");
 
   playBtn.addEventListener("click", playVideo);
   stopBtn.addEventListener("click", stopVideo);
+  slowBtn.addEventListener("click", speedUpVideo);
+  speedBtn.addEventListener("click", slowDownVideo);
 
   function playVideo() {
     if (video.paused === true) {
       video.play();
-      playBtn.style.backgroundImage = "url(img/pause_black.svg)";
+      playBtn.style.backgroundImage = "url(img/pause.svg)";
     } else {
       video.pause();
-      playBtn.style.backgroundImage = "url(img/play_black.svg)";
+      playBtn.style.backgroundImage = "url(img/play.svg)";
     }
   }
   function stopVideo() {
     video.pause();
     video.currentTime = 0;
-    playBtn.style.backgroundImage = "url(img/play_black.svg)";
+    playBtn.style.backgroundImage = "url(img/play.svg)";
   }
+
+  function slowDownVideo() {
+      if (video.playbackRate >= 0.25) {
+        video.playbackRate -= 0.25;
+        playBckRate.textContent = video.playbackRate;
+      }
+    }
+    function speedUpVideo() {
+      video.playbackRate += 0.25;
+      playBckRate.textContent = video.playbackRate;
+    }
 }
